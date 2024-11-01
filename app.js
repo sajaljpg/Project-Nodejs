fd9cc1dee51aa85416009fe9d8c64c9ba61fc622
sajal kumar <148806306+sajaljpg@users.noreply.github.com>
2024-11-01 22:09:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // connect to mongodb
 const dbURI='mongodb+srv://netninja:test1234@nodetuts.j6njy.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=nodetuts';
-mongoose.connect(dbURI, { useNewUrlParser: true })
+mongoose.connect(dbURI)
 
 
 .then((result) => app.listen(3000))
